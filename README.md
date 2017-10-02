13704030d36eb18ae8ecb6a3b8362e9eeff68511
ayyll <upyh@foxmail.com>
2017-10-02 14:53:16 +0800

    一些改动和说明

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # proxy_ip_spider
 一个爬取网页上可用的代理ip的python爬虫。
+
+几点说明：
+
+1.爬虫用的是BeautifulSoup库，简洁小巧，其实用的字段不多，自己正则去一点一点截也可以。
+
+2.模拟http请求用到了urllib以及requests，后者功能相对多一点，但是都差不多，其中一者即可满足本文需求。
+
+3.还有一个问题就是爬取中文资源显示乱码的问题，可以导入sys，然后：
+
+```
+reload(sys)
+sys.setdefaultencoding('utf8')
+```
+
+即可解决中文乱码问题。
--- change_ip_http_request.py
+++ change_ip_http_request.py
@@ -7,10 +7,10 @@ import bs4
 import socket
 import urllib
 import os
+
 # 检查url地址
-
-
 def check_link(url):
+    #模拟请求头
     User_Agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0'
     header = {}
     header['User-Agent'] = User_Agent
@@ -20,8 +20,7 @@ def check_link(url):
         r.encoding = r.apparent_encoding
         return r.text
     except:
-        print('无法链接服务器！！！')
-
+        print "无法连接服务器!"
 
 # 爬取资源
 def get_contents(ulist, rurl):
@@ -35,38 +34,33 @@ def get_contents(ulist, rurl):
         ulist.append(ui)
 
 # 保存资源
-
-
 def save_contents(urlist, proxy):
     for i in range(1, len(urlist)):
         proxy.append(urlist[i][11] + "://" + urlist[i][3] + ":" + urlist[i][5])
-'''''
-验证获得的代理IP地址是否可用
-'''
 
-
+#验证获得的代理IP地址是否可用
 def validateIp(proxy):
     url = "http://dddcc2.com/intr/c13a4715b1dfe3c1"
     socket.setdefaulttimeout(3)
     for i in range(0, len(proxy)):
-	    try:
-	    	if proxy[i][4] == 'S':
-	    		proxy_temp = {"https": proxy[i]}
-	    	else:
-	    		proxy_temp = {"http": proxy[i]}
-	        # params = {"bs":"study2017","vipid":"15157436","come_url":""}
-	        # params = urllib.urlencode(params)
-	        res = urllib.urlopen(url, proxies=proxy_temp).read()
-	        print proxy[i]
-	    except Exception, e:
-	        print "The IP Address is Useless"
-
+        try:
+            if proxy[i][4] == 'S':
+                proxy_temp = {"https": proxy[i]}
+            else:
+                proxy_temp = {"http": proxy[i]}
+            # params = {"bs":"study2017","vipid":"15157436","come_url":""}
+            # params = urllib.urlencode(params)
+            res = urllib.urlopen(url, proxies=proxy_temp).read()
+            print proxy[i] + "---可用";
+        except Exception, e:
+            print "The IP Address is Useless"
 
 def main():
     reload(sys)
     sys.setdefaultencoding('utf8')
     urli = []
     proxy = []
+    #待爬取的代理ip网址
     url = "http://www.xicidaili.com/nn/1"
     rs = check_link(url)
     get_contents(urli, rs)
@@ -74,26 +68,3 @@ def main():
     validateIp(proxy)
 
 main()
-'''
-import urllib2
-import requests
-from lxml import etree
- 
-proxy = {"http":"http://119.164.181.239:8118"}
-test_url="http://aaaoo9.com/intr/c13a4715b1dfe3c1"   #ip网站测试 http://ip.filefab.com/index.php
-resp=urllib2.urlopen(test_url).read()
-response = etree.HTML(resp)
-ip_addr = response.xpath('//div/h1[@id="ipd"]/span/text()')
-print "Before switching the IP address:",ip_addr
-# 使用代理IP地址之前的访问IP地址
-try:
-    response = requests.get(test_url)
-    response = etree.HTML(resp)
-    ip_addr = response.xpath('//div/h1[@id="ipd"]/span/text()')
-    print response.text
-    print "Now the IP ADDRESS IS:",ip_addr
-    # 使用代理IP地址之后的访问IP地址
-except Exception:
-    print "The IP Address is Useless" #代理IP不可用
-
-'''
